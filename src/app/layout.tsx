b88a3f5a8bf8e5e01b9d6f517a0e364983afac37
Nsanzumuhire Daniel <nsanzudaniel59@gmail.com>
2025-02-16 14:37:46 +0200

    ft: add verce analitics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from '@vercel/analytics/next';
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
@@ -51,6 +52,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <Header />
         {children}
+        <Analytics mode="production" />;
         <Footer />
       </body>
     </html>
